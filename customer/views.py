eeacb60d9467fa47dbcc45b7f6ec0dbb421d8cf6
Fahim Adnan <muhammadfahim010@gmail.com>
2024-05-19 15:37:36 +0600

    hello world

--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
 
 def hello_world(request: HttpRequest):
     return render(request, 'customer/hello_world.html')
