bd739a52bd233207b8e39adf29e7868082b6d885
Fahim Adnan <muhammadfahim010@gmail.com>
2024-05-22 17:09:09 +0600

    added restaurant login register

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def login(request):
+    return render(request, 'restaurant/login.html')
+
+
+def register(request):
+    return render(request, 'restaurant/register.html')
