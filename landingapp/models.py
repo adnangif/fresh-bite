3d6e0b64ad29763e5284351a5a797cd3e014ca18
Fahim Adnan <muhammadfahim010@gmail.com>
2024-05-25 01:30:54 +0600

    landing page design finish

--- landingapp/models.py
+++ landingapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CoveredCity(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+
+    city_image = models.ImageField(upload_to='city_images/', null=True, blank=True)
+    wiki_link = models.URLField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Covered City'
+        verbose_name_plural = 'Covered Cities'
+
+    def __str__(self):
+        return self.name
--- landingapp/views.py
+++ landingapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .models import CoveredCity
+
 
 def landing_page(request):
-    return render(request, 'landing/landing.html')
+
+    covered_cities: CoveredCity = CoveredCity.objects.all()[:10]
+    context = {
+        "covered_cities": covered_cities
+    }
+
+    return render(request, 'landing/landing.html', context)
