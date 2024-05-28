749960c6874821e1cbebf7dfd64a76b06c32abc8
Fahim Adnan <muhammadfahim010@gmail.com>
2024-05-29 01:12:31 +0600

    added page list

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('edit-restaurant/', views.edit_restaurant, name='edit_restaurant'),
+
+    path('menus/', views.menus, name='menus'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
@@ -9,3 +10,10 @@ def login(request):
 
 def register(request):
     return render(request, 'restaurant/register.html')
+
+def edit_restaurant(request: HttpRequest):
+    return render(request,'restaurant/edit-restaurant.html')
+
+
+def menus(request: HttpRequest):
+    return render(request, 'restaurant/menu-list.html')
