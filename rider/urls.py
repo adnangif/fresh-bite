ec12cc9601b32c5d1377bb4ad30141d3727322de
Fahim Adnan <muhammadfahim010@gmail.com>
2024-06-04 10:35:38 +0600

    connecting

--- rider/urls.py
+++ rider/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from . import  views
+from . import views
+
+app_name = 'rider'
+
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
--- user/urls.py
+++ user/urls.py
@@ -1,18 +1,22 @@
 
 from django.urls import path
 from . import views
+
+
+app_name = 'user'
+
 urlpatterns = [
     path('hello-world/', views.hello_world),
-    path('nearby/', views.nearby_restaurants),
-    path('restaurants/<int:restaurant_id>/', views.restaurant),
-    path('review-order/<int:order_id>/', views.review_order),
-    path('live-chat/', views.livechat),
-    path('track-orders/', views.track_orders),
-    path('faq/', views.faq),
-    path('feedback/', views.feedback),
-    path('login/', views.login),
-    path('register/', views.register),
-    path('edit-profile/', views.edit_profile),
-    path('rate/', views.rate),
+    path('nearby/', views.nearby_restaurants, name='nearby_restaurants'),
+    path('restaurants/<int:restaurant_id>/', views.restaurant, name='restaurant'),
+    path('review-order/<int:order_id>/', views.review_order, name='review_order'),
+    path('live-chat/', views.livechat, name='livechat'),
+    path('track-orders/', views.track_orders, name='track_orders'),
+    path('faq/', views.faq, name='faq'),
+    path('feedback/', views.feedback, name='feedback'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('edit-profile/', views.edit_profile, name='edit_profile'),
+    path('rate/', views.rate, name='rate'),
 
 ]
