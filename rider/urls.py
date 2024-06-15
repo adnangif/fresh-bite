542d9697b9796a1d23620e1b804b906d9f7f642e
Fahim Adnan <muhammadfahim010@gmail.com>
2024-06-15 13:46:06 +0600

    rider login register

--- rider/urls.py
+++ rider/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'rider'
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
     path('edit-profile/', views.edit_profile, name='edit_profile'),
     path('track-orders/', views.track_orders, name='track_orders'),
 ]
--- rider/views.py
+++ rider/views.py
@@ -1,15 +1,49 @@
+from django.contrib.auth import login, authenticate
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+
+from modelapp.models import Rider
 
 
+# def login(request: HttpRequest) -> HttpResponse:
+#
+#     return render(request, 'rider/login.html')
 
-def login(request: HttpRequest) -> HttpResponse:
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'rider/login.html')
 
-    return render(request, 'rider/login.html')
+    def post(self, request):
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        rider = authenticate(email=email, password=password)
+        if rider and rider.role == 'rider':
+            login(request, rider)
+
+            return redirect('landingapp:landing_page')
+
+        return render(request, 'rider/login.html', {'error': 'Invalid email or password.'})
 
 
-def register(request: HttpRequest) -> HttpResponse:
-    return render(request, 'rider/register.html')
+class RegisterView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'rider/register.html')
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            rider = Rider.objects.create_user(first_name=first_name, last_name=last_name, email=email, password=password)
+            login(request, rider)
+            return redirect('landingapp:landing_page')
+        except Exception as e:
+            print(e)
+            return render(request, 'rider/register.html', {'error': str(e)})
 
 
 def edit_profile(request: HttpRequest) -> HttpResponse:
