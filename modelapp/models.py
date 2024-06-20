10c7f40897e76147d85f25f9879317b5ec9180c9
Fahim Adnan <muhammadfahim010@gmail.com>
2024-06-20 16:55:19 +0600

    Update models.py

--- modelapp/models.py
+++ modelapp/models.py
@@ -1,3 +1,5 @@
+from xml.dom.minidom import Entity
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -68,3 +70,81 @@ class Rider(Person):
     def save(self, *args, **kwargs):
         self.role = Roles.RIDER
         super(Rider, self).save(*args, **kwargs)
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='items', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.menu) + " -> " + self.name + " " + str(self.price)
+
+
+class Location(models.Model):
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    entity = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.entity.email) + " -> " + str(self.latitude) + " " + str(self.longitude)
+
+
+class Wallet(models.Model):
+    entity = models.ForeignKey(Person, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True, default=0)
+
+    def __str__(self):
+        return self.entity.email + " -> " + str(self.amount)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="User")
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name="Owner")
+    rider = models.ForeignKey(Rider, on_delete=models.CASCADE, related_name="Rider")
+
+    status = models.CharField(max_length=100, blank=True, null=True)
+
+    def total_amount(self):
+        items = OrderItem.objects.filter(order=self)
+        amount = 0.0
+
+        for item in items:
+            amount += item.price * item.quantity
+
+        return amount
+
+
+
+    def __str__(self):
+        return (
+                str(self.owner.email) + " -> "
+                + str(self.rider.email) + " "
+                + str(self.user.email) + " "
+                + str(self.status)
+                )
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.item.name) + " -> " + str(self.quantity)
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    opens_at = models.TimeField(blank=True, null=True)
+    closes_at = models.TimeField(blank=True, null=True)
+
